a6d50c00afd5c7e91f4c77d6559034d55ab393a9
Fatima Nguyen <fatima3455@hotmail.com>
2020-08-27 18:48:38 +0000

    refactor with unit tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,19 @@
 const request = require('request');
-const breed = process.argv[2];
-const domain = 'https://api.thecatapi.com/v1/breeds/search?q=' + breed;
-request(domain, (error, response, body) => {
-  if (error) {
-    return console.log(error);
-  }
 
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    return console.log('The requested breed was not found.');
-  }
-  console.log(data[0].description);
-});
+const fetchBreedDescription = (breedName, cb) => {
+  const domain = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
+  request(domain, (error, response, body) => {
+    if (error) {
+      cb(error, null);
+    } else {
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        cb('The requested breed was not found', null);
+      } else {
+        cb(null, data[0].description);
+      }
+    }
+  });
+};
+
+module.exports = {fetchBreedDescription};
